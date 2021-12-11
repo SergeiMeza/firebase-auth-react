d500ac8e62c80f8db436bbcdebf2fda3f2507950
SergeiMeza <sergeimeza@gmail.com>
2021-12-11 17:25:57 +0900

    compile

--- lib/esm/auth/useSendEmailVerification.d.ts
+++ lib/esm/auth/useSendEmailVerification.d.ts
@@ -1,7 +1,7 @@
 import { FirebaseError } from 'firebase/app';
 import { ActionCodeSettings, User } from 'firebase/auth';
 export default function useSendEmailVerification(): readonly [
-    (user: User, actionCodeSettings: ActionCodeSettings) => Promise<void>,
+    (user: User, actionCodeSettings?: ActionCodeSettings | null | undefined) => Promise<void>,
     boolean,
     FirebaseError | undefined
 ];
